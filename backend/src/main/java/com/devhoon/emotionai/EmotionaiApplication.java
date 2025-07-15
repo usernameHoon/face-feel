a7fa15fcada672b6258693d2c61dad7f061e319c
usernameHoon <gns326@naver.com>
2025-07-15 19:47:05 +0900

    애플리케이션 실행 및 테스트 클래스 추가

---
+++ backend/src/main/java/com/devhoon/emotionai/EmotionaiApplication.java
@@ -0,0 +1,13 @@
+package com.devhoon.emotionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devhoon/emotionai/EmotionaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhoon.emotionai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
